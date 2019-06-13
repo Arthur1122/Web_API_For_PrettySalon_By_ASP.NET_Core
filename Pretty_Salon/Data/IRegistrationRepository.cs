02f2192ef8248b485b924929d88ab74d8762f129
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-06-13 11:28:07 +0400

    Add Interface for Entities.

---
+++ Pretty_Salon/Data/IRegistrationRepository.cs
@@ -0,0 +1,34 @@
+﻿using Pretty_Salon.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pretty_Salon.Data
+{
+    public interface IRegistrationRepository
+    {
+        //General
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveChangesAsync();
+
+        //Registrations
+        Task<Registration[]> GetAllRegistrationsAsync();
+        Task<Registration> GetRegistrationByNameAsync(string name);
+        Task<Registration[]> GetAllRegistrationsByDateAsync(DateTime dateTime);
+
+        //Clients
+        Task<Client[]> GetAllClientsAsync();
+        Task<Client> GetClientByNameAsync(string name);
+
+        //Hairdressers
+        Task<Hairdresser[]> GetAllHairdressersAsync();
+        Task<Hairdresser> GetHairdresserByNameAsync(string name);
+
+        //Salons
+        Task<Salon[]> GetAllSalonsAsync();
+        Task<Salon> GetSalonByNameAsync(string name);
+
+    }
+}
